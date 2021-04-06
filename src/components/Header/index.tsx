9834fced3f2e098689e6810ff7f7681a5575ef52
ropoko <rodrigostramantinoli@gmail.com>
2021-04-06 18:19:37 -0300

    Added Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { StyledHeader } from './HeaderStyle';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
+
+interface Props {
+    toggleTheme(): void;
+}
+
+const Header: React.FC<Props> = ({toggleTheme}) => {
+    const { colors, title } = useContext(ThemeContext);
+
+    return(
+        <StyledHeader>
+            <strong> Hello from header component </strong>
+            <Switch 
+                onChange={toggleTheme} 
+                checked={title === 'dark'}
+                checkedIcon={false}
+                uncheckedIcon={false}
+                height={10}
+                width={40}
+                handleDiameter={20}
+                offColor={colors.secundary}
+                onColor={colors.secundary}
+            />
+        </StyledHeader>
+    )
+}
+
+export default Header;
