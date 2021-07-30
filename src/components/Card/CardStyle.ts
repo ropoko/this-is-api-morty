22f21bd518a194b0301d99455c1aa5672bf347df
ropoko <rodrigostramantinoli@gmail.com>
2021-07-29 21:12:01 -0300

    styled card component

--- src/components/Card/CardStyle.ts
+++ src/components/Card/CardStyle.ts
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
 export const StyledCard = styled.div`
-    color: red;
+    margin: auto;
+    padding: 1.6rem;
+    align-self: center;
+    justify-self: center;
+
+    header img {
+        border-radius: 5px;
+    }
+
+    main {
+        text-align: center;
+        background-color: ${props => props.theme.colors.card.background};
+        color: ${props => props.theme.colors.card.title};
+        border-radius: 0 0 10px 10px;
+        padding: 0.4rem;
+    }
 `;
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,13 +9,12 @@ interface Props {
 const Card: React.FC<Props> = (rick) => {
   return (
     <StyledCard>
-        <h3>Hello from Card</h3>
-        <div className="image-container">
-            <img src={rick.image} alt="" />
-        </div>
-        <div className="details">
-            <h1>{rick.name}</h1>
-        </div>
+        <header>
+          <img src={rick.image} alt="" />
+        </header>
+        <main>
+          <h3>{rick.name}</h3>
+        </main>
     </StyledCard>
   );
 };
